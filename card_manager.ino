03894cf71b36f4cc136d6fea95816db604370b6c
Timothy Batt <jammincoderguy96@gmail.com>
2023-03-30 11:14:37 -0400

    optimized read/write

--- card_manager.ino
+++ card_manager.ino
@@ -8,11 +8,10 @@ MFRC522 mfrc(SS_PIN, RST_PIN);
 
 void setup() 
 {
-  Serial.begin(115200); 
-  delay(1500);
+  Serial.begin(9600); 
   Serial.println("STARTING UP");
+
   SPI.begin();
-  
   mfrc.PCD_Init(); 
   Serial.println("\nScan your card.");
   Serial.println();
@@ -20,15 +19,15 @@ void setup()
 
 void loop() 
 {
+  delay(100);
   // Wait for card, exit if no card
   if (!mfrc.PICC_IsNewCardPresent()) return;
   if (!mfrc.PICC_ReadCardSerial()) return;
+  Serial.println("HERE!");
 
   // Card availible, read it.
-  String rfid_data = rfid_read(&mfrc);
-  String parsed_rfid = parse_card_id(rfid_data);
-
-  Serial.println(parsed_rfid);
+  String id = rfid_read(&mfrc);
+  Serial.println(id);
 
   // Stop RFID device
   mfrc.PICC_HaltA(); 
--- rfid.h
+++ rfid.h
@@ -2,23 +2,11 @@
 #include <SPI.h>
 
 #define RFID_SIZE_BUFFER     18
-#define RFID_MAX_SIZE_BLOCK  16
+#define RFID_MAX_BLOCK_SIZE  16
 
 
 MFRC522::MIFARE_Key key;
 MFRC522::StatusCode status;
-
-String remove_invalid_chars(String str) {
-  String result;
-  for (int i = 0; i < str.length(); i++) {
-    char c = str.charAt(i);
-    if (c >= 0 && c < 128) result += c;
-  }
-
-  result = result.substring(0, result.indexOf(' '));
-  result.trim();
-  return result;
-}
 
 
 String rfid_read(MFRC522* mfrc)
@@ -39,9 +27,6 @@ String rfid_read(MFRC522* mfrc)
   if (status != MFRC522::STATUS_OK) {
     Serial.print(F("Authentication failed: "));
     Serial.println(mfrc->GetStatusCodeName(status));
-    digitalWrite(redPin, HIGH);
-    delay(1000);
-    digitalWrite(redPin, LOW);
     return;
   }
 
@@ -50,25 +35,17 @@ String rfid_read(MFRC522* mfrc)
   if (status != MFRC522::STATUS_OK) {
     Serial.print(F("Reading failed: "));
     Serial.println(mfrc->GetStatusCodeName(status));
-    digitalWrite(redPin, HIGH);
-    delay(1000);
-    digitalWrite(redPin, LOW);
     return;
   }
   else{
       Serial.println("Card OK");
   }
 
-  return remove_invalid_chars((String)(char*)buffer);
+  return String((char*)buffer).substring(0, 16);
 }
 
 
-String parse_card_id(String data) {
-  // card data: name=some_name
-  return data.substring(data.indexOf('=') + 1);
-}
-
-void rfid_write(MFRC522* mfrc) {
+void rfid_write(MFRC522* mfrc, String id, byte block = 1) {
   // mfrc->PICC_DumpDetailsToSerial(&(mfrc->uid)); 
   
   // Waits 30 seconds dor data entry via Serial 
@@ -79,15 +56,12 @@ void rfid_write(MFRC522* mfrc) {
   for (byte i = 0; i < 6; i++) key.keyByte[i] = 0xFF;
 
   // Buffer for storing data to write
-  byte buffer[RFID_MAX_SIZE_BLOCK] = "";
-  byte block = 1;
-  byte dataSize;
-
-  // Recover on buffer the data from Serial all characters before chacactere '#'
-  dataSize = Serial.readBytesUntil('#', (char*)buffer, RFID_MAX_SIZE_BLOCK);
+  byte buffer[RFID_MAX_BLOCK_SIZE] = "";
+  
+  for (int i = 0; i < RFID_SIZE_BUFFER; i++) buffer[i] = id[i];
 
   // Void positions that are left in the buffer will be filled with whitespace
-  for(byte i=dataSize; i < RFID_MAX_SIZE_BLOCK; i++) buffer[i] = ' ';
+  for(byte i = id.length(); i < RFID_MAX_BLOCK_SIZE; i++) buffer[i] = ' ';
  
   String str = (char*)buffer;
   Serial.println(str);
@@ -99,20 +73,14 @@ void rfid_write(MFRC522* mfrc) {
   if (status != MFRC522::STATUS_OK) {
     Serial.print(F("PCD_Authenticate() failed: "));
     Serial.println(mfrc->GetStatusCodeName(status));
-    digitalWrite(redPin, HIGH);
-    delay(1000);
-    digitalWrite(redPin, LOW);
     return;
   }
  
   // Writes in the block
-  status = mfrc->MIFARE_Write(block, buffer, RFID_MAX_SIZE_BLOCK);
+  status = mfrc->MIFARE_Write(block, buffer, RFID_MAX_BLOCK_SIZE);
   if (status != MFRC522::STATUS_OK) {
     Serial.print(F("MIFARE_Write() failed: "));
     Serial.println(mfrc->GetStatusCodeName(status));
-    digitalWrite(redPin, HIGH);
-    delay(1000);
-    digitalWrite(redPin, LOW);
     return;
   }
   else{
